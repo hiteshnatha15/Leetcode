b8b725684e971ebe38acafa0071932b0c88a6f3f
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-29 15:48:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (17.42%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -15,10 +15,10 @@ public:
                     sub++;
                 }
             }
-            if (sub > k)
-                low = mid + 1;
-            else
+            if (sub <= k)
                 high = mid - 1;
+            else 
+                low = mid + 1;
         }
         return low;
     }
