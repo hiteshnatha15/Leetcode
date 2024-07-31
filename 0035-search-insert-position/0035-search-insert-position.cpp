8da81980491991272ad88739462aea693bb20b15
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-31 22:30:19 +0530

    Time: 3 ms (71.3%), Space: 12.4 MB (19.99%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,13 +4,18 @@ public:
         int ans = nums.size();
         int low = 0;
         int high = nums.size() - 1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] >= target) {
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target) {
+                ans = mid + 1;
+                low = mid + 1;
+            } else {
                 ans = mid;
                 high = mid - 1;
-            } else
-                low = mid + 1;
+            }
         }
         return ans;
     }
