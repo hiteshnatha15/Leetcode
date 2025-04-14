c5982d0e4b073f59bded13a739a5532e3b02a1a6
Hitesh Natha <hiteshnatha15@gmail.com>
2025-04-14 20:48:18 +0530

    Time: 2 ms (53.31%), Space: 8.5 MB (92.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long ans=0;
-        int temp=x;
-        if(x<0)
-        return false;
-        while(temp){
-            ans=(ans*10)+temp%10;
-            temp/=10;
+        int rev = 0;
+        int temp = x;
+        if (x < 0)
+            return false;
+        while (temp) {
+            if(rev>INT_MAX/10)
+                return false;
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
         }
-        return x==ans;
+        return x == rev;
     }
 };
