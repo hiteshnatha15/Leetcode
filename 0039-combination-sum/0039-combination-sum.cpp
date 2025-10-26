220390a95d097ef92be0ec5bd991b30e0ff33b87
Hitesh Natha <hiteshnatha15@gmail.com>
2025-10-26 11:18:40 +0530

    Time: 0 ms (100%), Space: 14.1 MB (53.71%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void helper(vector<vector<int>>& ans, vector<int>& curr, int idx, int sum,
+                vector<int>& candidates, int target) {
+        if (target == sum) {
+            ans.push_back(curr);
+            return;
+        }
+        if (idx == candidates.size() || sum > target)
+            return;
+        curr.push_back(candidates[idx]);
+        helper(ans, curr, idx, sum + candidates[idx], candidates, target);
+        curr.pop_back();
+        helper(ans, curr, idx + 1, sum, candidates, target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(ans, curr, 0, 0, candidates, target);
+        return ans;
+    }
+};
