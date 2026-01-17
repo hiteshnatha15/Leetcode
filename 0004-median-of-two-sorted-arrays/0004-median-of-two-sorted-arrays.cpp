cb3a5b4f9677f6617878c6ce8aa28ff5c5a4d22f
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-17 12:39:59 +0530

    Time: 3 ms (29.62%), Space: 94.9 MB (93.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        for (int i = 0; i < nums1.size(); i++) {
-            res.push_back(nums1[i]);
+        int i = 0;
+        int j = 0;
+        int idx1 = (nums1.size() + nums2.size()) / 2;
+        int idx2 = idx1 - 1;
+        int ele1 = 0;
+        int ele2 = 0;
+        int cnt = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                if (cnt == idx1)
+                    ele1 = nums1[i];
+                if (cnt == idx2)
+                    ele2 = nums1[i];
+                i++;
+            } else {
+                if (cnt == idx1)
+                    ele1 = nums2[j];
+                if (cnt == idx2)
+                    ele2 = nums2[j];
+                j++;
+            }
+            cnt++;
         }
-        for (int i = 0; i < nums2.size(); i++) {
-            res.push_back(nums2[i]);
+        while (i < nums1.size()) {
+            if (cnt == idx1)
+                ele1 = nums1[i];
+            if (cnt == idx2)
+                ele2 = nums1[i];
+            i++;
+            cnt++;
         }
-        sort(res.begin(),res.end());
-        int n=res.size();
-        if(n%2==0)
-            return (res[n/2-1]+res[(n)/2])/2.0;
-        return double(res[(n)/2]);
+        while (j < nums2.size()) {
+            if (cnt == idx1)
+                ele1 = nums2[j];
+            if (cnt == idx2)
+                ele2 = nums2[j];
+            j++;
+            cnt++;
+        }
+
+        if ((nums1.size() + nums2.size()) % 2 != 0)
+            return (double)ele1;
+        return (double)(ele1 + ele2) / 2.0;
     }
 };
