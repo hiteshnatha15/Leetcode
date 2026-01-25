588cbfd4f8cd2419010cee80c494e6a215bfa728
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-25 15:54:35 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
             int mid = (left + right) / 2;
             if (nums[mid] == target)
                 return mid;
-            if (nums[left] <= nums[mid]) {
+            else if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target <= nums[mid])
                     right = mid - 1;
                 else
